e7abac8cd1fd64352b34caa53a71c445e4107ce5
Franco Perez <franco.perez03@gmail.com>
2016-03-24 09:02:02 -0300

    andan las imagenes

--- app/models/admin/product.rb
+++ app/models/admin/product.rb
@@ -6,7 +6,7 @@ class Admin::Product < ActiveRecord::Base
   validates :codigo , presence: {message: " no fue ingresado"} , uniqueness: {message: "ya existe"}
   validates :precio , presence: {message: " no fue ingresado"} , numericality: {message: "debe ser un numero"}
 
-  has_attached_file :image, styles:{ main: '300x400>', thumb:'50x50>' }
+  has_attached_file :image, styles: { medium: '300x400>', thumb: '60x60>' }
 	validates_attachment :image, :presence => true,
     :content_type => { :content_type => "image/jpeg" },
     :size => { :in => 0..2000.kilobytes }

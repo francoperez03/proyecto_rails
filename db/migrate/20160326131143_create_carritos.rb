3ad1f785a5cf0fd35597c8f78f0b13992b4db33f
Franco Perez <franco.perez03@gmail.com>
2016-03-26 15:47:22 -0300

    nuevo heroku

--- db/migrate/20160326131143_create_carritos.rb
+++ db/migrate/20160326131143_create_carritos.rb
@@ -2,7 +2,7 @@ class CreateCarritos < ActiveRecord::Migration
   def change
     create_table :carritos do |t|
       t.references :user, index: true, foreign_key: true
-      t.references :product, index: true, foreign_key: true
+      t.references :admin_product, index: true, foreign_key: true
     end
   end
 end

201c6fb388b4bd3fe172f5dfc64d09f54b0ae9ee
Franco Perez <franco.perez03@gmail.com>
2016-03-26 15:45:42 -0300

    nuevo heroku

--- db/migrate/20160322233147_add_category_to_admin_products.rb
+++ db/migrate/20160322233147_add_category_to_admin_products.rb
@@ -1,6 +1,6 @@
 class AddCategoryToAdminProducts < ActiveRecord::Migration
   def change
-    add_reference :admin_products, :category, index: true, foreign_key: true
-    add_reference :admin_products, :marca, index: true, foreign_key: true
+    add_reference :admin_products, :admin_category, index: true, foreign_key: true
+    add_reference :admin_products, :admin_marca, index: true, foreign_key: true
   end
 end
